c09f8731f1295325d299fa1a9126e4a17920d49b
Sergio Ruiz <sergivar24@gmail.com>
2023-02-20 11:37:42 +0100

    debug de animacion

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const barraNavegacion = document.getElementById('barraDeNavegacion');
 const mensajeBienvenida = document.getElementById('logoRestaurante');
 const localizacion = document.location;
 
+alert(localizacion.pathname);
 
 let timer;
 let opacidadLogo=1;
