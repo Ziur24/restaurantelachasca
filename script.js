ae23f7955e12b391f45d2871aecaf3cdce6daaf9
Sergio Ruiz <sergivar24@gmail.com>
2023-02-20 12:47:58 +0100

    debug script  error en header al cambiar de pagina

--- script.js
+++ script.js
@@ -10,24 +10,21 @@ let opacidadBarraNavegacion=0;
 let etapaBinevenida = 1;
 
 window.addEventListener('load', function(){
-  console.log("Hola, el pathname es:" + localizacion.pathname);
+  particionCadena("hola/como/estas/loco");
   timer = this.setInterval(cambioOpacidad, 15)
 });
 
 
-
 function cambioOpacidad(){
-
+  
   if (etapaBinevenida==1 && localizacion.pathname=="/home.html"){
     
     if (opacidadLogo>0.0){
     opacidadLogo=opacidadLogo-0.01;
     contenedor.style.opacity=opacidadLogo;
     }else if(opacidadBarraNavegacion<1){
-    
       opacidadBarraNavegacion=opacidadBarraNavegacion+0.01;
       barraNavegacion.style.opacity=opacidadBarraNavegacion;
-  
     }else{
       etapaBinevenida=2; 
     }
@@ -37,11 +34,18 @@ function cambioOpacidad(){
       mensajeBienvenida.innerHTML="Bienvenidos";
       opacidadLogo=opacidadLogo+0.01;
       contenedor.style.opacity=opacidadLogo;
+      }else if(etapaBinevenida==2 && opacidadLogo==1){
+      clearInterval(timer);
       }
-  }else if(localizacion.pathname=="/carta.html" || localizacion.pathname=="/contacto.html"){
+  }else if(particionCadena(localizacion.pathname)=="carta.html" || particionCadena(localizacion.pathname)=="contacto.html"){
     barraNavegacion.style.opacity=1;
   }
+  
 }
 
-
-
+function particionCadena(cadena){
+  let separador='/';
+  let arrayParticion = cadena.split(separador);
+  let ultimoComponente = arrayParticion[arrayParticion.length-1];
+  return ultimoComponente;
+}
